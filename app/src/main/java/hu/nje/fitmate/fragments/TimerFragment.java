fc0c4dc334c9daa0dd4dee2be1c063212ddafd5c
Dani <szabodani8@gmail.com>
2024-03-23 08:45:57 +0100

    Timer UI 2

---
+++ app/src/main/java/hu/nje/fitmate/fragments/TimerFragment.java
@@ -0,0 +1,26 @@
+package hu.nje.fitmate.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import hu.nje.fitmate.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link TimerFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class TimerFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_timer, container, false);
+    }
+}
